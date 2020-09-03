dcf718366f964e00e023cd256a34c3214dfdc7da
kenny0619 <kehindeibukun0619@gmail.com>
2020-09-03 22:37:04 +0100

    Setup Environment Variable

---
+++ app/db/dev/dbQuery.js
@@ -0,0 +1,22 @@
+const pool = require("./pool");
+
+module.exports = {
+  /**
+   * DB Query
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} object
+   */
+  query(quertText, params) {
+    return new Promise((resolve, reject) => {
+      pool
+        .query(quertText, params)
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+};
