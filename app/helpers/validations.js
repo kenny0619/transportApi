3d7aaf0bdd1fb18aada38f36df99565896926494
kenny0619 <kehindeibukun0619@gmail.com>
2020-09-03 23:20:40 +0100

    setup helpers

---
+++ app/helpers/validations.js
@@ -0,0 +1,50 @@
+const env = require("../../env");
+
+/**
+ * isValidEmail helper method
+ * @param {string} email
+ * @returns {Boolean} True or False
+ */
+const isValidEmail = (email) => {
+  const regEx = /\S+@\S+\.\S+/;
+  return regEx.test(email);
+};
+
+/**
+ * validatePassword helper method
+ * @param {string} password
+ * @returns {Boolean} True or False
+ */
+const validatePassword = (password) => {
+  if (password.length <= 7 || password === "") {
+    return false;
+  }
+  return true;
+};
+/**
+ * isEmpty helper method
+ * @param {string, integer} input
+ * @returns {Boolean} True or False
+ */
+const isEmpty = (input) => {
+  if (input === undefined || input === "") {
+    return true;
+  }
+  if (input.replace(/\s/g, "").length) {
+    return false;
+  }
+  return true;
+};
+
+/**
+ * empty helper method
+ * @param {string, integer} input
+ * @returns {Boolean} True or False
+ */
+const empty = (input) => {
+  if (input === undefined || input === "") {
+    return true;
+  }
+};
+
+module.exports = { isValidEmail, validatePassword, isEmpty, empty };
